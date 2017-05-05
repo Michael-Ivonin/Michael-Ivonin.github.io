bbf42c96a60c45d32d7703876cb4fc8c49cc391c
Michael Ivonin <Michael@drummer.ru>
2017-05-05 14:42:59 +0600

    Correction for footer

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Michael on 05.05.2017.
+ */
+
+$(document).ready(function() {
+  if ($(window).height() > $("body").height()) {
+    $(".footer").addClass("navbar-fixed-bottom");
+  }
+});
